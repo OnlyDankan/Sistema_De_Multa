fd47210751a0a790387ede92055894ea9a2e31b3
Daniel <marcosdanielsferreira2015@gmail.com>
2026-05-14 19:58:21 -0300

    .

--- Backend/obj/Debug/net10.0/need for speed.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/need for speed.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("need for speed")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44c5b761f7bb78286dd6158f290aebce509a98dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+182e362c42d05dcd49a7c9f96c3defc59b6ecaaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("need for speed")]
 [assembly: System.Reflection.AssemblyTitleAttribute("need for speed")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
