a15563323ac5d40ce49c86225ea2d928d149bf49
Daniel <marcosdanielsferreira2015@gmail.com>
2026-05-27 10:32:18 -0300

    .

--- SistemaDeMulta/obj/Debug/net10.0/SistemaDeMulta.AssemblyInfo.cs
+++ SistemaDeMulta/obj/Debug/net10.0/SistemaDeMulta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeMulta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ff4838173010d32f1ad19f5f1764f5ed8e56c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32111cfec662b63cb786aec3dd01d4947aeb5386")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeMulta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeMulta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
